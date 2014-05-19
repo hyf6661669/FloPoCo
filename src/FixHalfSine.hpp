de133ef55d5c435c25e4854e0b3eee302ec05cd0
David Thomas <d.thomas1@imperial.ac.uk>
2014-05-19 20:03:04 +0000

    Gradual merge with trunk contd.

--- src/FixHalfSine.hpp
+++ src/FixHalfSine.hpp
@@ -1,5 +1,5 @@
-#ifndef FIXDCT_HPP
-#define FIXDCT_HPP
+#ifndef FIXHALFSINE_HPP
+#define FIXHALFSINE_HPP
 
 #include "Operator.hpp"
 #include "utils.hpp"
@@ -24,39 +24,38 @@
 
 namespace flopoco{
 
-	class FixDCT2 : public Operator
+	class FixHalfSine : public Operator
 	{
 	public:
 
-		FixDCT2(Target* target, int p_, int N_, int k_, bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
-		
+		FixHalfSine(Target* target, int p_, int N_, bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
+
 		/*
-		FixDCT(Operator* parentOp, Target* target, int p_, int N_, int k_,
+		FixOQPSK(Operator* parentOp, Target* target, int p_, int N_,
 							 BitHeap* bitheap,
 							 bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
 		*/
-		~FixDCT2(){};
+		~FixHalfSine(){};
 
 		// Overloading the virtual functions of Operator
 		/* the emulate function is used to simulate in software the operator
 		   in order to compare this result with those outputed by the vhdl opertator */
 		void emulate(TestCase* tc);
-		
+
 		/* function used to create Standard testCase defined by the developper */
 		void buildStandardTestCases(TestCaseList* tcl);
-		
+
 		//TODO: revert back to returning an mpfr_t
-		long double getDCT2FilterCoeff(int i);
-		string getDCT2FilterCoeffString(int i);
-		
-		
-		int p;  					/**< The precision of inputs and outputs */ 
+		long double getHalfSineFilterCoeff(int i);
+		string getHalfSineFilterCoeffString(int i);
+
+
+		int p;  					/**< The precision of inputs and outputs */
 		int N;  					/**< The number of taps */
-		int k;  					/**< The number of the current sample */
-		
+
 		bool signedInput;			/**< The inputs are signed, or not */
-		
-		mpfr_t mpcoeff[10000];  	/**< The coefficients of the DCT2 filter, as MPFR numbers */
+
+		mpfr_t mpcoeff[10000];  	/**< The coefficients of the OQPSK filter, as MPFR numbers */
 		vector<string> coeff;		/**< the coefficients as strings */
 
 		int wO;  					/**< output size, will be computed out of the constants */
--- src/FixRRCF.hpp
+++ src/FixRRCF.hpp
@@ -1,5 +1,5 @@
-#ifndef FIXDCT_HPP
-#define FIXDCT_HPP
+#ifndef FIXRRCF_HPP
+#define FIXRRCF_HPP
 
 #include "Operator.hpp"
 #include "utils.hpp"
@@ -24,39 +24,39 @@
 
 namespace flopoco{
 
-	class FixDCT2 : public Operator
+	class FixRRCF : public Operator
 	{
 	public:
 
-		FixDCT2(Target* target, int p_, int N_, int k_, bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
-		
+		FixRRCF(Target* target, int p_, int N_, double alpha_ = 1, bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
+
 		/*
-		FixDCT(Operator* parentOp, Target* target, int p_, int N_, int k_,
+		FixRRCF(Operator* parentOp, Target* target, int p_, int N_, double aplha_ = 1,
 							 BitHeap* bitheap,
 							 bool signedInput_ = false, map<string, double> inputDelays = emptyDelayMap);
 		*/
-		~FixDCT2(){};
+		~FixRRCF(){};
 
 		// Overloading the virtual functions of Operator
 		/* the emulate function is used to simulate in software the operator
 		   in order to compare this result with those outputed by the vhdl opertator */
 		void emulate(TestCase* tc);
-		
+
 		/* function used to create Standard testCase defined by the developper */
 		void buildStandardTestCases(TestCaseList* tcl);
-		
+
 		//TODO: revert back to returning an mpfr_t
-		long double getDCT2FilterCoeff(int i);
-		string getDCT2FilterCoeffString(int i);
-		
-		
-		int p;  					/**< The precision of inputs and outputs */ 
+		long double getFixRRCFilterCoeff(int i);
+		string getFixRRCFilterCoeffString(int i);
+
+
+		int p;  					/**< The precision of inputs and outputs */
 		int N;  					/**< The number of taps */
-		int k;  					/**< The number of the current sample */
-		
+		double alpha;					/**< The roll-of factor */
+
 		bool signedInput;			/**< The inputs are signed, or not */
-		
-		mpfr_t mpcoeff[10000];  	/**< The coefficients of the DCT2 filter, as MPFR numbers */
+
+		mpfr_t mpcoeff[10000];  	/**< The coefficients of the RRC filter, as MPFR numbers */
 		vector<string> coeff;		/**< the coefficients as strings */
 
 		int wO;  					/**< output size, will be computed out of the constants */

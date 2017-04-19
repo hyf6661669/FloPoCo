2561c0bfb50b820fd3726eddde5d3312714012ad
Martin Kumm <kumm@animal.digi.e-technik.uni-kassel.de>
2017-04-19 17:08:42 +0200

    BaseMultiplierDSP added

---
+++ src/IntMult/BaseMultiplierDSP.cpp
@@ -0,0 +1,71 @@
+#include "BaseMultiplierDSP.hpp"
+#include "../PrimitiveComponents/Xilinx/Xilinx_LUT6.hpp"
+#include "../PrimitiveComponents/Xilinx/Xilinx_CARRY4.hpp"
+#include "../PrimitiveComponents/Xilinx/Xilinx_LUT_compute.h"
+
+namespace flopoco {
+
+
+BaseMultiplierDSP::BaseMultiplierDSP(bool isSignedX, bool isSignedY, int width, bool flipXY) : BaseMultiplier(isSignedX,isSignedY)
+{
+
+    srcFileName = "BaseMultiplierDSP";
+    uniqueName_ = "BaseMultiplierDSP";
+
+    this->flipXY = flipXY;
+
+    if(!flipXY)
+    {
+        wX = 2;
+        wY = width;
+    }
+    else
+    {
+        wX = width;
+        wY = 2;
+    }
+
+}
+
+Operator* BaseMultiplierDSP::generateOperator(Target* target)
+{
+    return new BaseMultiplierDSPOp(target, isSignedX, isSignedY, wX, wY, flipXY);
+}
+	
+
+BaseMultiplierDSPOp::BaseMultiplierDSPOp(Target* target, bool isSignedX, bool isSignedY, int wX, int wY, bool flipXY) : Operator(target)
+{
+    ostringstream name;
+    name << "BaseMultiplierDSP";
+    setName(name.str());
+
+    string in1,in2;
+
+    if(!flipXY)
+    {
+        this->wX = wX;
+        this->wY = wY;
+        in1 = "X";
+        in2 = "Y";
+    }
+    else
+    {
+        this->wX = wY;
+        this->wY = wX;
+        in1 = "Y";
+        in2 = "X";
+    }
+
+    addInput(in1, wX, true);
+    addInput(in2, wY, true);
+
+    if((isSignedX == true) || (isSignedY == true)) throw string("unsigned inputs currently not supported by BaseMultiplierDSPOp, sorry");
+
+    wR = wX + wY; //ToDo: addjust this for signed
+    addOutput("R", wR);
+
+    vhdl << tab << "R <= std_logic_vector(unsigned(X) * unsigned(Y));" << endl;
+}
+
+}   //end namespace flopoco
+

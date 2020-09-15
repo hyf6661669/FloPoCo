4c57893736b7c523b57cf1d02a9993cbfee086d1
fdedinec <Florent.de-Dinechin@insa-lyon.fr>
2020-09-15 14:15:40 +0200

    factoring code out

--- src/Tables/DiffCompressedTable.cpp
+++ src/Tables/DiffCompressedTable.cpp
@@ -17,24 +17,19 @@ namespace flopoco {
 		string subsamplingIn = "X_subsampling";
 		vhdl << tab << declare(subsamplingIn, t.subsamplingIndexSize) << " <= " << "X" << range(wIn-1, wIn-t.subsamplingIndexSize) << ";" << endl;
 		string subsamplingOut = "Y_subsampling";
-		string diffout = "Y_diff";
+		string diffOut = "Y_diff";
 		Table::newUniqueInstance(this, subsamplingIn, subsamplingOut,
 														 t.subsampling, getName()+"_subsampling",
 															t.subsamplingIndexSize, t.subsamplingWordSize, _logicTable );
 		// generate VHDL for diff table
-		Table::newUniqueInstance(this, "X", diffout,
+		Table::newUniqueInstance(this, "X", diffOut,
 															t.diffs, getName()+"_diff",
 														 wIn, t.diffWordSize,
 														 _logicTable);
 
-		int nonOverlapBits = wOut-t.diffWordSize;
-		int overlapBits    = t.subsamplingWordSize - nonOverlapBits;
-		// TODO an intadder when needed, but this is proably never useful since this addition is so small
-		vhdl << tab << declare(getTarget()->adderDelay(t.subsamplingWordSize),
-													 "Y_topbits", t.subsamplingWordSize) << " <= " << subsamplingOut			
-		<< " + (" << zg(nonOverlapBits) << "& (" <<diffout << range(t.diffWordSize-1,t.diffWordSize-overlapBits) << "));" << endl;
-		vhdl << tab << declare("fullOut", wOut) << " <= " << "Y_topbits & (" <<diffout << range(t.diffWordSize-overlapBits-1,0) << ");" << endl;		
+		t.insertAdditionVHDL(this, "fullOut", subsamplingOut, diffOut);
 		vhdl << tab << "Y <= fullOut;" << endl;
+
 	}
 
 		OperatorPtr DiffCompressedTable::newUniqueInstance(OperatorPtr op,

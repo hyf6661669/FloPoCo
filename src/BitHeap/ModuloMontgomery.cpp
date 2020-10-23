b7a44c0c919bc32229b7f58576deca86b1802040
Annika Oeste <annika.oeste@informatik.hs-fulda.de>
2020-10-23 16:13:59 +0200

    use montgomery reduction with montgomery multiplication and only add constants when needed

--- src/BitHeap/ModuloMontgomery.cpp
+++ src/BitHeap/ModuloMontgomery.cpp
@@ -30,51 +30,92 @@ namespace flopoco {
         // declaring inputs
         addInput ("X" , wIn, true);
         addConstant("M", "positive", modulo);
-        addConstant("Rmod", "integer", R % modulo);
         // declaring output
         addOutput("S" , modSize);
 
-        REPORT(INFO, "rmod: " << R % modulo);
         addFullComment("Start of vhdl generation");
-        if(method == "modOp") {
-            // calculating residue to use montgomery
-            // conversion to signed
-            vhdl << tab << declareFixPoint(
-                    "XSgTemp", false, wIn-1, 0) << tab << "<= UNSIGNED(STD_LOGIC_VECTOR(UNSIGNED(X) mod " << modulo << ")" << ");" << endl;
-            vhdl << tab << declareFixPoint(
-                    "XSg", false, modSize-1, 0) << tab << "<= XSgTemp" << range(modSize-1, 0) << ";" << endl;
-            vhdl << tab << declareFixPoint(
-                    "RmodSg", false, modSize-1, 0) << tab << "<= UNSIGNED(TO_SIGNED(Rmod," << modSize << "));" << endl;
-            // calculate and convert back
-            vhdl << tab << declare(
-                    "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << modSize*2 << " => '0') & STD_LOGIC_VECTOR(XSg * RmodSg);" << endl;
-        } else if (method == "ex") {
-            // classical modular multiplication
-            // use shift because R is power of two
-            vhdl << tab << declare(
-                    "XRProd", wIn + modSize, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_LEFT(RESIZE(UNSIGNED(X)," << wIn + modSize << ")," << modSize << "));" << endl;
-            // use division
-            ostringstream divParams;
-            divParams << "wIn=" << wIn + modSize << " d=" << modulo << " computeQuotient=false computeRemainder=true";
-            newInstance("IntConstDiv", "modDiv", divParams.str(), "X=>XRProd", "R=>XRemain");
-            vhdl << tab << declare(
-                    "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << modSize << " => '0') & XRemain;" << endl;
-        } else if (method == "redOnly") {
-            if (wIn > modSize*2) {
-                THROWERROR("Parameter wIn is too big for reduction only test. Can only be twice the number of bits of the modulo.");
+
+        if (method == "modOp" || method == "ex" || method == "redOnly") {
+            // methods that convert the number before reduction to montgomery form
+            if (method == "modOp") {
+                addConstant("Rmod", "integer", R % modulo);
+                REPORT(DEBUG, "rmod: " << R % modulo);
+                // calculating residue to use montgomery
+                // conversion to signed
+                vhdl << tab << declareFixPoint(
+                        "XSgTemp", false, wIn-1, 0) << tab << "<= UNSIGNED(STD_LOGIC_VECTOR(UNSIGNED(X) mod " << modulo << ")" << ");" << endl;
+                vhdl << tab << declareFixPoint(
+                        "XSg", false, modSize-1, 0) << tab << "<= XSgTemp" << range(modSize-1, 0) << ";" << endl;
+                vhdl << tab << declareFixPoint(
+                        "RmodSg", false, modSize-1, 0) << tab << "<= UNSIGNED(TO_SIGNED(Rmod," << modSize << "));" << endl;
+                // calculate and convert back
+                vhdl << tab << declare(
+                        "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << modSize*2 << " => '0') & STD_LOGIC_VECTOR(XSg * RmodSg);" << endl;
+            } else if (method == "ex") {
+                // classical modular multiplication
+                // use shift because R is power of two
+                vhdl << tab << declare(
+                        "XRProd", wIn + modSize, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_LEFT(RESIZE(UNSIGNED(X)," << wIn + modSize << ")," << modSize << "));" << endl;
+                // use division
+                ostringstream divParams;
+                divParams << "wIn=" << wIn + modSize << " d=" << modulo << " computeQuotient=false computeRemainder=true";
+                newInstance("IntConstDiv", "modDiv", divParams.str(), "X=>XRProd", "R=>XRemain");
+                vhdl << tab << declare(
+                        "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << modSize << " => '0') & XRemain;" << endl;
+            } else if (method == "redOnly") {
+                if (wIn > modSize*2) {
+                    THROWERROR("Parameter wIn is too big for reduction only test. Can only be twice the number of bits of the modulo.");
+                }
+
+                vhdl << tab << declare(
+                        "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << wIn << " => '0') & X;" << endl;
             }
 
+            // montgomery reduction - automatically converts back
+            for (int i = 0; i < modSize; ++i) {
+                ostringstream tName;
+                int index = i+1;
+                tName << "T_" << index;
+                vhdl << tab << declare(
+                        tName.str(), modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(UNSIGNED(T_" << i
+                     << ") + SHIFT_LEFT(TO_UNSIGNED(M," << modSize+i << ")," << i << ")) when T_" << i << of(i) << " = '1' else T_" << i << ";" << endl;
+            }
             vhdl << tab << declare(
-                    "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << wIn << " => '0') & X;" << endl;
+                    "TResTemp", modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_RIGHT(UNSIGNED(T_" << modSize << ")," << modSize << "))"  << ";" << endl;
+            vhdl << tab << declare(
+                    "TRes", modSize+1, false) << tab << " <= TResTemp" << range(modSize, 0) << ";" << endl;
+            vhdl << tab << declare("TResMM", modSize+1, false) << "<= STD_LOGIC_VECTOR(UNSIGNED(TRes) - M)" << ";" << endl;
+            vhdl << tab << "S <= TRes" << range(modSize-1, 0) << " when UNSIGNED(TRes) < M else TResMM" << range(modSize-1, 0) << ";" << endl;
+            addFullComment("End of vhdl generation");
         } else if (method == "multi") {
-            if (wIn > modSize) {
-                THROWERROR("Parameter wIn is too big for montgomery multiplication. Cannot be more than the number of bits of the modulo.");
+            // method that uses montgomery multiplication after the reduction to convert the result back
+            if (wIn > modSize*2) {
+                THROWERROR("Parameter wIn is too big for montgomery reduction. Can only be twice the number of bits of the modulo.");
             }
 
             int conversionMultiplicand = (1 << (2*modSize)) % modulo;
-            addInput ("B" , wIn, true);
             addConstant("conMult", "natural", conversionMultiplicand);
-            // montgomery multiplication
+            // montgomery reduction
+            vhdl << tab << declare(
+                    "T_0", modSize*2*R, false) << tab << "<= (" << modSize*2*R-1 << " downto " << wIn << " => '0') & X;" << endl;
+
+            for (int i = 0; i < modSize; ++i) {
+                ostringstream tName;
+                int index = i+1;
+                tName << "T_" << index;
+                vhdl << tab << declare(
+                        tName.str(), modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(UNSIGNED(T_" << i
+                     << ") + SHIFT_LEFT(TO_UNSIGNED(M," << modSize+i << ")," << i << ")) when T_" << i << of(i) << " = '1' else T_" << i << ";" << endl;
+            }
+            vhdl << tab << declare(
+                    "TResTemp", modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_RIGHT(UNSIGNED(T_" << modSize << ")," << modSize << "))"  << ";" << endl;
+            vhdl << tab << declare(
+                    "TRes", modSize+1, false) << tab << " <= TResTemp" << range(modSize, 0) << ";" << endl;
+            vhdl << tab << declare("TResMM", modSize+1, false) << "<= STD_LOGIC_VECTOR(UNSIGNED(TRes) - M)" << ";" << endl;
+            vhdl << tab << declare(
+                    "RedRes", modSize, false) << tab << " <= TRes" << range(modSize-1, 0) << " when UNSIGNED(TRes) < M else TResMM" << range(modSize-1, 0) << ";" << endl;
+
+            // montgomery multiplication to convert back from montgomery form
             vhdl << tab << declare(
                     "R_0", modSize*2, false) << tab << "<= (" << modSize*2-1 << " downto 0 => '0');" << endl;
             for (int i = 0; i < modSize; ++i) {
@@ -82,7 +123,7 @@ namespace flopoco {
                 int index = i+1;
                 rTmp1Name << "Rtmp1_" << i;
                 vhdl << tab << declare(
-                        rTmp1Name.str(), modSize*2, false) << tab << "<= R_" << i << " when X" << of(i) << " = '0' else STD_LOGIC_VECTOR(UNSIGNED(R_" << i << ") + UNSIGNED(B));" << endl;
+                        rTmp1Name.str(), modSize*2, false) << tab << "<= R_" << i << " when RedRes" << of(i) << " = '0' else STD_LOGIC_VECTOR(UNSIGNED(R_" << i << ") + conMult);" << endl;
                 ostringstream rTmp2Name;
                 rTmp2Name << "Rtmp2_" << i;
                 vhdl << tab << declare(
@@ -92,51 +133,12 @@ namespace flopoco {
                 vhdl << tab << declare(
                         rTmp3Name.str(), modSize*2, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_RIGHT(UNSIGNED(Rtmp2_" << i << "),1));" << endl;
             }
-            vhdl << tab << declare(
-                    "MultRes", modSize, false) << tab << "<= R_" << modSize << range(modSize-1,0) << " when UNSIGNED(R_" << modSize << ") < M";
-            vhdl << " else STD_LOGIC_VECTOR(UNSIGNED(R_" << modSize << range(modSize-1,0) << ") - M);" << endl;
-            // montgomery multiplication to convert back from montgomery form
-            vhdl << tab << declare(
-                    "R2_0", modSize*2, false) << tab << "<= (" << modSize*2-1 << " downto 0 => '0');" << endl;
-            for (int i = 0; i < modSize; ++i) {
-                ostringstream rTmp1Name;
-                int index = i+1;
-                rTmp1Name << "R2tmp1_" << i;
-                vhdl << tab << declare(
-                        rTmp1Name.str(), modSize*2, false) << tab << "<= R2_" << i << " when MultRes" << of(i) << " = '0' else STD_LOGIC_VECTOR(UNSIGNED(R2_" << i << ") + conMult);" << endl;
-                ostringstream rTmp2Name;
-                rTmp2Name << "R2tmp2_" << i;
-                vhdl << tab << declare(
-                        rTmp2Name.str(), modSize*2, false) << tab << "<= R2tmp1_" << i << " when R2tmp1_" << i << of(0) << " = '0' else STD_LOGIC_VECTOR(UNSIGNED(R2tmp1_" << i << ") + M);" << endl;
-                ostringstream rTmp3Name;
-                rTmp3Name << "R2_" << index;
-                vhdl << tab << declare(
-                        rTmp3Name.str(), modSize*2, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_RIGHT(UNSIGNED(R2tmp2_" << i << "),1));" << endl;
-            }
-            vhdl << tab << "S <= R2_" << modSize << range(modSize-1,0) << " when UNSIGNED(R2_" << modSize << ") < M";
-            vhdl << tab << "else STD_LOGIC_VECTOR(UNSIGNED(R2_" << modSize << range(modSize-1,0) << ") - M);" << endl;
+            vhdl << tab << "S <= R_" << modSize << range(modSize-1,0) << " when UNSIGNED(R_" << modSize << ") < M";
+            vhdl << tab << "else STD_LOGIC_VECTOR(UNSIGNED(R_" << modSize << range(modSize-1,0) << ") - M);" << endl;
             addFullComment("End of vhdl generation");
-            return;
         } else {
             REPORT(INFO, "invalid method name " << method);
         }
-
-        // montgomery reduction - automatically converts back
-        for (int i = 0; i < modSize; ++i) {
-            ostringstream tName;
-            int index = i+1;
-            tName << "T_" << index;
-            vhdl << tab << declare(
-                    tName.str(), modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(UNSIGNED(T_" << i
-                 << ") + SHIFT_LEFT(TO_UNSIGNED(M," << modSize+i << ")," << i << ")) when T_" << i << of(i) << " = '1' else T_" << i << ";" << endl;
-        }
-        vhdl << tab << declare(
-                "TResTemp", modSize*2*R, false) << tab << "<= STD_LOGIC_VECTOR(SHIFT_RIGHT(UNSIGNED(T_" << modSize << ")," << modSize << "))"  << ";" << endl;
-        vhdl << tab << declare(
-                "TRes", modSize+1, false) << tab << " <= TResTemp" << range(modSize, 0) << ";" << endl;
-        vhdl << tab << declare("TResMM", modSize+1, false) << "<= STD_LOGIC_VECTOR(UNSIGNED(TRes) - M)" << ";" << endl;
-        vhdl << tab << "S <= TRes" << range(modSize-1, 0) << " when UNSIGNED(TRes) < M else TResMM" << range(modSize-1, 0) << ";" << endl;
-        addFullComment("End of vhdl generation");
     }
 
     int ModuloMontgomery::getModuloMSB() {
@@ -166,9 +168,6 @@ namespace flopoco {
             int rInverse = getModularInverse((1 << modSize), modulo);
             mpz_class sNormal = (sx * rInverse) % modulo;
             sr = sNormal % modulo;
-        } else if (method == "multi") {
-            mpz_class sb = tc->getInputValue("B");
-            sr = (sx * sb) % modulo;
         } else {
             sr = sx % modulo;
         }
@@ -182,53 +181,35 @@ namespace flopoco {
         if (wIn >= 10) {
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(634));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
         }
         if (wIn >= 8) {
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(222));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
         }
         if (wIn >= 7) {
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(120));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
         }
         if (wIn >= 6) {
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(33));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
 
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(40));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
         }
         if (wIn >= 4) {
             tc = new TestCase(this);
             tc->addInput("X", mpz_class(15));
-            if (method == "multi") {
-                tc->addInput("B", mpz_class(1));
-            }
             emulate(tc);
             tcl->add(tc);
         }
@@ -253,17 +234,15 @@ namespace flopoco {
                         }
                         modSize++;
                     }
-                    else {
-                        for (int wIn=6; wIn<=12; wIn++) {
-                            if (!(methods[i]=="redOnly" && wIn > modSize * 2)) {
-                                paramList.push_back(make_pair("wIn",to_string(wIn)));
-                                paramList.push_back(make_pair("modulo",to_string(m)));
-                                paramList.push_back(make_pair("method", methods[i]));
-                                paramList.push_back(make_pair("TestBench n=",to_string(100)));
-                                testStateList.push_back(paramList);
+                    for (int wIn=6; wIn<=12; wIn++) {
+                        if (!(methods[i]=="redOnly" && wIn > modSize * 2)) {
+                            paramList.push_back(make_pair("wIn",to_string(wIn)));
+                            paramList.push_back(make_pair("modulo",to_string(m)));
+                            paramList.push_back(make_pair("method", methods[i]));
+                            paramList.push_back(make_pair("TestBench n=",to_string(100)));
+                            testStateList.push_back(paramList);
 
-                                paramList.clear();
-                            }
+                            paramList.clear();
                         }
                     }
                 }
@@ -277,7 +256,7 @@ namespace flopoco {
                     }
                 }
                 modSize++;
-                paramList.push_back(make_pair("wIn",to_string(modSize)));
+                paramList.push_back(make_pair("wIn",to_string(modSize*2)));
                 paramList.push_back(make_pair("modulo",to_string(m)));
                 paramList.push_back(make_pair("method", "multi"));
                 paramList.push_back(make_pair("TestBench n=",to_string(100)));
@@ -312,7 +291,7 @@ namespace flopoco {
                             method(string)=ex: The method used for the montgomery algrithm."
                             " 4 available: 'ex' - for explicit conversion, 'modOp' - for Conversion using the vhdl mod operator,"
                             " 'redOnly' - the input is directly used for the reduction without conversion in montgomery form,"
-                            " 'multi' - using the montgomery multiplication algorithm",
+                            " 'multi' - using the montgomery reduction + montgomery multiplication to convert the result back",
                 // More documentation for the HTML pages. If you want to link to your blog, it is here.
                            "See the developer manual in the doc/ directory of FloPoCo.",
                            ModuloMontgomery::parseArguments,

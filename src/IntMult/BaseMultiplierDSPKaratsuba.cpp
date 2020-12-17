866f0ad857807e95415c86e72f746990433174d6
x-aboett <andreas-daniel.boettcher@cs.hs-fulda.de>
2020-12-17 15:35:09 +0100

    Disable faulty calculation of the cost reduction due to protruding bits within the Karatsuba pattern multiplier tiles to avoid the incorrect calculation of negative costs for those multipliers.

--- src/IntMult/BaseMultiplierDSPKaratsuba.cpp
+++ src/IntMult/BaseMultiplierDSPKaratsuba.cpp
@@ -86,15 +86,15 @@ namespace flopoco {
     }
 
     double BaseMultiplierDSPKaratsuba::getLUTCost(int x_anchor, int y_anchor, int wMultX, int wMultY){
-        int bits = 0, add_bits = 0, protruding_bits;                //TODO: Check handling of protruding bits
+        int bits = 0, add_bits = 0, protruding_bits = 0;                //TODO: Check handling of protruding bits
         int gcd = BaseMultiplierDSPKaratsuba::gcd(wX, wY);
         long kxy = gcd;
         for(; kxy % wX || kxy % wY; kxy += gcd);
         for(int j = 0; j <= n; j++){
             for(int i = 0; i <= j; i++){
-                protruding_bits = x_anchor+y_anchor+2*kxy*j+wX+wY - wMultX+wMultY;
-                cout << protruding_bits << endl;
-                protruding_bits =((protruding_bits < 0)?0:protruding_bits);
+                //protruding_bits = x_anchor+y_anchor+2*kxy*j+wX+wY - wMultX+wMultY;
+                //cout << protruding_bits << endl;
+                //protruding_bits =((protruding_bits < 0)?0:protruding_bits);
                 bits += (wX + wY - protruding_bits);
                 if(i != j) {          //the DSPs that are replaced by Karatsuba contribute with 3 bits to bit heap
                     bits += 2 * (wX + wY - protruding_bits);

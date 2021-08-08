b786213b780abf45e6df6999ac92ab84fc2e9f4e
Andreas Boettcher <andreas.boettcher@informatik.hs-fulda.de>
2021-08-08 18:08:31 +0200

    Consider that the output width of the 2x1- or 2x1-multiplier is 1 bit larger (3 bit instead of 2) in the cost function, when the short or both sides are signed.

--- src/IntMult/BaseMultiplierLUT.cpp
+++ src/IntMult/BaseMultiplierLUT.cpp
@@ -20,15 +20,18 @@ Operator* BaseMultiplierLUT::generateOperator(
 }
 
 	double BaseMultiplierLUT::getLUTCost(int x_anchor, int y_anchor, int wMultX, int wMultY, bool signedIO){
-		int ws = (wX()==1)?wY():((wY()==1)?wX():wX()+wY());
+        bool signedX = signedIO && (wMultX == x_anchor+wX());
+        bool signedY = signedIO && (wMultY == y_anchor+wY());
+		//int ws = (wX()==1)?wY():((wY()==1)?wX():wX()+wY());
+		int ws = (wX()==1 && !signedX)?wY():((wY()==1 && !signedY)?wX():wX()+wY());     //The wOut of 2x1 or 2x1 is 1 bit larger, when the short or both sides are signed.
 		int luts = ((ws <= 5)?ws/2+ws%2:ws);
 
 		int x_min = ((x_anchor < 0)?0: x_anchor);
 		int y_min = ((y_anchor < 0)?0: y_anchor);
 		int lsb = x_min + y_min;
 
-		int x_max = ((wMultX < x_anchor + (int)wX())?wMultX: x_anchor + wX());
-		int y_max = ((wMultY < y_anchor + (int)wY())?wMultY: y_anchor + wY());
+		int x_max = ((wMultX < x_anchor + (int)wX())?wMultX: x_anchor + (int)wX());
+		int y_max = ((wMultY < y_anchor + (int)wY())?wMultY: y_anchor + (int)wY());
 		int msb = x_max+y_max;
 		ws = (x_max-x_min==1)?y_max-y_min:((y_max-y_min==1)?x_max-x_min:msb - lsb);
 
@@ -36,7 +39,10 @@ Operator* BaseMultiplierLUT::generateOperator(
 	}
 
 	int BaseMultiplierLUT::ownLUTCost(int x_anchor, int y_anchor, int wMultX, int wMultY, bool signedIO) {
-		int ws = (wX()==1)?wY():((wY()==1)?wX():wX()+wY());
+        bool signedX = signedIO && (wMultX == x_anchor+wX());
+        bool signedY = signedIO && (wMultY == y_anchor+wY());
+        //int ws = (wX()==1)?wY():((wY()==1)?wX():wX()+wY());
+        int ws = (wX()==1 && !signedX)?wY():((wY()==1 && !signedY)?wX():wX()+wY());
 		int luts = ((ws <= 5)?ws/2+ws%2:ws);
 		return luts;
 	}
